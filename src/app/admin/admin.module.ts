f07969bff819a4205bbcde7a6c3ba2d7bcdd5d56
Evelyn Zepeda <evaynnn@gmail.com>
2024-07-10 15:55:43 -0700

    adding untracked

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { AdminRoutingModule } from "./admin-routing.module";
+import { AdminComponent } from "./admin.component";
+import { ViewEmployeesComponent } from "./view-employees/view-employees.component";
+import { CreateEmployeesComponent } from "./create-employees/create-employees.component";
+import { DeleteEmployeesComponent } from "./delete-employees/delete-employees.component";
+import { EditEmployeesComponent } from "./edit-employees/edit-employees.component";
+import { RouterModule } from "@angular/router";
+
+@NgModule({
+  declarations: [
+    AdminComponent,
+    ViewEmployeesComponent,
+    CreateEmployeesComponent,
+    DeleteEmployeesComponent,
+    EditEmployeesComponent
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  exports: [
+    AdminComponent,
+    ViewEmployeesComponent,
+    CreateEmployeesComponent,
+    DeleteEmployeesComponent,
+    EditEmployeesComponent,
+    RouterModule
+  ]
+})
+
+export class AdminModule {}
