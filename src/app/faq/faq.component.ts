5a1f728d078aa83bacca7fe6a16870ab7afc781e
DeVonte <devonte.ellis@gmail.com>
2024-07-12 23:17:04 -0500

    Sprint 2 FAQ
    FAQ page added, html, css, and typescript

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+/**
+ * DeVonte' Ellis
+ * 7-11-24
+ * FAQ page logic and functionality
+ */
+
+import { Component, OnInit } from '@angular/core';;
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.css']
 })
-export class FaqComponent {
 
+export class FaqComponent implements OnInit {
+  constructor() {}
+//Adds the required functionality for opening the faq panels
+  ngOnInit(): void {
+    const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach(faq => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("active")
+    })
+})
+  }
 }
+
+
+
